8588fdbef7b6d224dd8c8b71771b7badf376f708
RAMIR007 <ramiroxavier07@gmail.com>
2025-09-03 02:22:06 -0400

    "Vistas básicas y URLs para listar productos y detalle"

---
+++ tienda_muebles/tienda/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'tienda'
+
+urlpatterns = [
+    path('', views.lista_productos, name='lista_productos'),
+    path('producto/<int:pk>/', views.detalle_producto, name='detalle_producto'),
+]
--- tienda_muebles/tienda/views.py
+++ tienda_muebles/tienda/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Producto
 
-# Create your views here.
+def lista_productos(request):
+    productos = Producto.objects.filter(disponible=True)
+    return render(request, 'tienda/lista_productos.html', {'productos': productos})
+
+def detalle_producto(request, pk):
+    producto = get_object_or_404(Producto, pk=pk, disponible=True)
+    return render(request, 'tienda/detalle_producto.html', {'producto': producto})
